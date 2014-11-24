09f91f3a51c312448cd500a75dc2b3920c498f5c
Dave Tarrant <davetaz@theodi.org>
2014-11-24 13:22:47 +0000

    minor fix to pull images from correct location and via whatever protocol

--- app/assets/javascripts/training-blocks.js
+++ app/assets/javascripts/training-blocks.js
@@ -13,8 +13,8 @@ function processCourses(courses,instances) {
 		occurs = getCourseInstances(instances,key);
 		heading = "<h2><a href='" + link + "'>" + title + "</a></h2><p class='courseSub'>" + subtitle + "</p>";
 		icons = '<ul class="course_properties">';
-		icons += '<li><img src="images/'+duration.replace(/ /g,"_")+'.png" alt="'+duration+'"></img></li>';
-		icons += '<li><img src="images/f2f.png" alt="Face to face"></img></li>';
+		icons += '<li><img src="//static.theodi.org/assets/training/'+duration.replace(/ /g,"_")+'.png" alt="'+duration+'"></img></li>';
+		icons += '<li><img src="//static.theodi.org/assets/training/f2f.png" alt="Face to face"></img></li>';
 		icons += '</ul>';
 		running = '<div class="instances">Upcoming: <small>('+occurs.length+')</small><ul>';
 		for(k=0;k<occurs.length;k++) {
